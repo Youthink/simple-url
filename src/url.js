69a01a0cf18fa2c155cd3bd83a476d253657d70b
杨明明 <ddmingmingyang@gmail.com>
2016-10-25 16:32:24 +0800

    Add structured hash support

--- src/url.js
+++ src/url.js
@@ -2,7 +2,7 @@ const qs =  require('qs');
 
 const urlRegex = /^(?:([^:/?#]+):(?=\/\/))?(?:\/\/(?:(.*?)@)?([^/?#]*)?)?([^?#]*)(?:\?([^#]*))?(?:#(.*))?/;
 
-const parseUrl = (url) => {
+const parseUrl = (url, structuredHash) => {
   let match = urlRegex.exec(url);
   return match && {
       protocol: match[1] || '',
@@ -10,22 +10,28 @@ const parseUrl = (url) => {
       host: match[3] || '',
       pathname: match[4] || '',
       query: qs.parse(match[5]),
-      hash: match[6] || ''
+      hash: structuredHash === true ? qs.parse(match[6]) : decodeURIComponent(match[6] || '')
   };
 };
 
 const url =  {
   parse: parseUrl,
-  create: (protocol, host, pathname, query, hash) => {
-    protocol = protocol || 'http';
-    host = host || 'localhost';
-    return `${protocol}://${host}${url.createPath(pathname, query, hash)}`;
+  create: (options, structuredHash) => {
+    options.protocol = options.protocol || 'http';
+    options.host = options.host || 'localhost';
+    return `${options.protocol}://${options.host}${url.createPath(options.pathname, options.query, options.hash, structuredHash)}`;
   },
-  createPath: (pathname, query, hash) => {
+  createPath: (pathname, query, hash, structuredHash) => {
+    if(typeof pathname === 'object' && pathname !== null){
+      query = pathname.query;
+      hash = pathname.hash;
+      pathname = pathname.pathname;
+      structuredHash = query;
+    }
     pathname = pathname || '/';
     pathname = pathname.charAt(0) === '/' ? pathname : '/' + pathname;
     query = query ? `?${qs.stringify(query)}` : '';
-    hash = hash ? `#${hash}` : '';
+    hash = hash ? structuredHash === true ? `#${qs.stringify(hash)}` : `#${encodeURIComponent(hash)}` : '';
     return pathname + query + hash;
   },
   qs: qs
